1ec80a3d1454028a1992bc156807b42523699391
sidney <shogo12000@hotmail.com>
2025-04-28 01:14:20 -0700

    create google OAUTH

--- auth.ts
+++ auth.ts
@@ -2,10 +2,12 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
+  providers: [Google],
+
+  callbacks: {
+    authorized: async ({ auth }) => {
+      // Logged in users are authenticated, otherwise redirect to login page
+      return !!auth
+    },
+  },
 })
